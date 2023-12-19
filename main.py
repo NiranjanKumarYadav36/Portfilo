1d9b868fab0e428d8d69338ead8914c9b1721210
sparkels36 <piyush362002@gmail.com>
2023-12-19 14:18:55 +0530

    render the app title in two columns using pandas

---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas
+import streamlit as st
+import pandas as pa
+
+
+st.set_page_config(layout='wide')
+
+col1, col2 = st.columns(2, gap='small')
+
+with col1:
+    st.image("images/photo1.jpg")
+
+# col2 =
+with col2:
+    st.title("Niranjan Kumar Yadav")
+    content = """ 
+    Hi, I am Niranjan.
+    """
+    st.info(content)
+
+
+content2 = """
+Below you can find some of the apps I have built in Pyton.Feel free to contact me!
+"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+
+df = pandas.read_csv("data.csv", sep=';')
+
+with col3:
+    for index, rows in df[:10].iterrows():
+        st.header(rows['title'])
+
+with col4:
+    for index, rows in df[10:].iterrows():
+        st.header(rows['title'])
+
